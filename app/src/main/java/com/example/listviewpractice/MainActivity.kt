bf3dab49d08dc897de070a98a102c0f1e71157c8
강진우 <wlsdn1252@naver.com>
2023-01-04 13:59:29 +0900

    학생목록 ArrayList생성, 실제 목록 추가

--- app/src/main/java/com/example/listviewpractice/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractice.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(Student("강진우",1997))
+        mStudentList.add(Student("정호석",1991))
+        mStudentList.add(Student("남민재",1992))
+        mStudentList.add(Student("백승민",1993))
+        mStudentList.add(Student("반나",1994))
+        mStudentList.add(Student("이봉준",1995))
+
     }
 }
